b5cc301c346b47a4dd03710f787580a0a16feac4
Erlingus <munguia.erling4@gmail.com>
2023-04-17 15:50:48 +0100

    adding image to the footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                   
                 </h1>
                 <a 
                     href="/blog"
@@ -18,11 +18,12 @@
         </div>
     </div>
 
-    <div style="display: flex;">
-        <img src="{{ asset('images/mariotunel.jpeg') }}" alt="" style="flex: 1; margin-right: 10px;">
-        <img src="{{ asset('images/mariotunel1.jpeg') }}" alt="" style="flex: 1; margin-left: 10px;">
-    </div>
-    
+   <div style="display: flex;">
+    <img src="{{ asset('images/mariotunel.jpeg') }}" alt="" style="flex: 1; margin-right: 10px; width: 100px; height: 350px;">
+    <img src="{{ asset('images/mariobros.jpg') }}" alt="" style="flex: 1; margin: auto; width: 130px; height: 350px;">
+    <img src="{{ asset('images/mariotunel1.jpeg') }}" alt="" style="flex: 1; margin-left: 10px; width: 100px; height: 350px;">
+</div>
+
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
             <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,10 @@
 <footer class="bg-gray-800 py-20 mt-20">
     <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+
+        <div style="display: flex;">
+            <img src="{{ asset('images/mario1.png') }}" alt="" style="flex: 1; margin-right: 10px; width: 100px; height: 350px;">
+            
+        </div>
         <div>
             <h3 class="text-l sm:font-bold text-gray-100">
                 Pages
