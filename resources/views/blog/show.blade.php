4cb44a35ae9ee02badf302be5c6c4b51be8846e1
Erlingus <munguia.erling4@gmail.com>
2023-05-09 10:57:40 +0100

    creating the first blog

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -14,9 +14,14 @@
         By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
     </span>
 
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+    {{-- <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
         {{ $post->description }}
-    </p>
+    </p> --}}
+
+    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light text-justify">
+  {{ $post->description }}
+</p>
+
 </div>
 
 @endsection 
